5c73246d73bc46f46389fc25fcbc1022b078d779
Tadas <118689974+Tadas37@users.noreply.github.com>
2022-12-05 18:44:31 +0200

    Update Project.asm

--- Project.asm
+++ Project.asm
@@ -3,29 +3,29 @@
 .DATA
 	buff db 255, 0, 255 dup(?)
 	endl db 0dh,0ah,'$'
-	message db'Iveskite simboliu eilute:',0dh,0ah,'$'
+	message db'Enter a symbol line:',0dh,0ah,'$'
 .CODE
 start:
     mov ax, @data
     mov ds, ax
 	
-	mov ah, 09h ;informacija vartotojui ka reikia ivesti
+	mov ah, 09h ;information for the user about what needs to be entered
     mov dx, offset message
     int 21h
 
-    mov ah, 0ah ;duomenu nuskaitymas is klaviaturos
+    mov ah, 0ah ;reading data from the keyboard
     mov dx, offset buff
     int 21h
 	
-	mov ah, 09h ;nauja eilute
+	mov ah, 09h ;new line
     mov dx, offset endl
     int 21h
 	
-	mov cl, [buff + 1] ;issaugomas simboliu kiekis cl'e
+	mov cl, [buff + 1] ;the amount of characters stored in the cl register
 	mov bl,cl
-	inc bl ;gaunamas paskutinis simbolis
+	inc bl ;the last character is obtained
 	
-ciklas1:
+loop1:
 	mov ax,20h
 	push ax
 	
@@ -51,7 +51,7 @@ ciklas1:
 	
 	dec bl
 	
-	loop ciklas1
+	loop loop1
 	
 	mov al, [buff + 1]
 	mov bl, 3h
@@ -60,10 +60,10 @@ ciklas1:
 	add cx, ax
 	mov ah,02h
 	
-ciklas2:
+loop2:
 	pop dx
 	int 21h
-	loop ciklas2
+	loop loop2
 
     mov ax, 4C00h
     int 21h
